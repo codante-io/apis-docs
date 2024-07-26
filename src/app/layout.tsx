5c5761940f6d3ab0b3b35bce8d814403f5f00d35
Roberto Cestari <robertotcestari@gmail.com>
2024-07-26 11:21:55 -0300

    remove protocol name

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -9,8 +9,8 @@ import { type Section } from '@/components/SectionProvider'
 
 export const metadata: Metadata = {
   title: {
-    template: '%s - Protocol API Reference',
-    default: 'Protocol API Reference',
+    template: '%s - Codante APIs',
+    default: 'Codante APIs - Docs',
   },
 }
 
